3fea466330960d4cdbbfca831027520811f305b2
onlee1425 <enflenffl@gmail.com>
2023-05-16 22:15:35 +0900

    init

---
+++ js/data.js
@@ -0,0 +1,174 @@
+/*
+'tutle'-ISTJ ,0
+'rhino'-ISFJ ,1
+'panda'-INFJ ,2
+'tiger'-INTJ ,3
+'snake'-ISTP ,4
+'cat'-ISFP ,5
+'seal'-INFP ,6
+'owl'-INTP ,7
+'hyena'-ESTP ,8
+'dolphin'-ESFP ,9
+'monkey'-ENFP ,10
+'parrot'-ENTP ,11
+'wolf'-ESTJ ,12
+'elephant'-ESFJ ,13
+'dog'-ENFJ ,14
+'rion'-ENTJ ,15
+*/
+
+const qnaList = [
+    {
+      q: 'test',
+      a: [
+        { answer: 'test', type: [1, 2, 4, 9] },
+        { answer: 'test', type: [0, 3, 6, 5, 11, 8] }
+      ]
+    },
+    {
+      q: 'test',
+      a: [
+        { answer: 'test', type: [0, 3, 2, 8] },
+        { answer: 'test', type: [1, 6, 5, 11] },
+        
+      ]
+    },
+    {
+      q: 'test',
+      a: [
+        { answer: 'test', type: [1, 3, 2, 11, 8] },
+        { answer: 'test', type: [7, 9, 12] },
+      ]
+    },
+    {
+      q: 'test',
+      a: [
+        { answer: 'test', type: [1, 2, 4 ] },
+        { answer: 'test', type: [7, 11, 8, 9, 12 ] },
+      ]
+    },
+    {
+      q: 'test',
+      a: [
+        { answer: 'test', type: [1, 7, 11, 4, 9 ] },
+        { answer: 'test', type: [0, 3, 6, 12]},
+      ]
+    },
+  
+    {
+      q: 'test',
+      a: [
+        { answer: 'test', type: [4, 9, 12 ] },
+        { answer: 'test', type: [0, 3, 6, 11 ] },
+      ]
+    },
+    {
+      q: 'test',
+      a: [
+        { answer: 'test', type: [1, 7, 12 ] },
+        { answer: 'test', type: [2, 4, 9 ] },
+      ]
+    },
+    {
+      q: 'test',
+      a: [
+        { answer: 'test', type: [0, 4, 9 ] },
+        { answer: 'test', type: [3, 2, 6, 5, 11, 8 ] },
+      ]
+    },
+    {
+      q: 'test',
+      a: [
+        { answer: 'test', type: [7, 0, 5, 9 ] },
+        { answer: 'test', type: [1, 3, 6, 12 ] },
+      ]
+    },
+    {
+      q: 'test',
+      a: [
+        { answer: 'test', type: [4, 9, 12] },
+        { answer: 'test', type: [0, 2, 6, 5 ] },
+      ]
+    },
+    {
+      q: 'test',
+      a: [
+        { answer: 'test', type: [1, 4, 10, 12 ] },
+        { answer: 'test', type: [7, 0, 3, 6, 11] },
+      ]
+    },
+    {
+      q: 'test',
+      a: [
+        { answer: 'test', type: [3, 6, 4, 10 ] },
+        { answer: 'test', type: [0, 2, 5, 11, 8] },
+      ]
+    }
+  ]
+  
+  const infoList = [
+    {
+      name: 'test <test>',
+      desc: 'test desc'
+    },
+    {
+      name: 'test <test>',
+      desc: 'test desc'
+    },
+    {
+      name: 'test <test>',
+      desc: 'test desc',
+    },
+    {
+      name: 'test <test>',
+      desc: 'test desc'
+    },
+    {
+      name: 'test <test>',
+      desc: 'test desc'
+    },
+    {
+      name: 'test <test>',
+      desc: 'test desc'
+    },
+    {
+      name: 'test <test>',
+      desc: 'test desc'
+    },
+    {
+      name: 'test <test>',
+      desc: 'test desc'
+    },
+    {
+      name: 'test <test>',
+      desc: 'test desc'    
+    },
+    {
+      name: 'test <test>',
+      desc: 'test desc'
+    },
+    {
+      name: 'test <test>',
+      desc: 'test desc'
+    },
+    {
+      name: 'test <test>',
+      desc: 'test desc'
+    },
+    {
+        name: 'test <test>',
+        desc: 'test desc'
+      },
+      {
+        name: 'test <test>',
+        desc: 'test desc'
+      },
+      {
+        name: 'test <test>',
+        desc: 'test desc'
+      },
+      {
+        name: 'test <test>',
+        desc: 'test desc'
+      },
+  ]
